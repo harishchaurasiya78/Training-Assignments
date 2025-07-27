6ed69b64ab2580ec5ecc2975fdae178b267fb57b
Harish Chaurasiya <harishbca2022@gmail.com>
2025-07-27 20:34:56 +0530

    Initial with all practice folders

---
+++ firstprogram/Hello.cs
@@ -0,0 +1,15 @@
+
+using System;
+
+class Hello
+{
+
+    static void Main(string[] args)
+    {
+
+        var i = 10;  // when we don't know the data type of a variable
+        var name = "Harish";
+
+        Console.WriteLine(i + " " + name);
+    }
+}
